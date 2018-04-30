2a3d5ab5528b49d31b9cb885ac2d48afc8159b52
Warren Reader <WarrenDReader@gmail.com>
2018-04-30 09:14:28 -0600

    Added isogram challenge and one solution.

---
+++ JavaScript/Code Challenges/Isograms.js
@@ -0,0 +1,16 @@
+//An isogram is a word that has no repeating letters, consecutive or non-consecutive. Creat a function that determines whether a string that contains only letters is an isogram. Assume that an empty string is an isogram. Ignore letter case.
+
+//Solution 1
+function isIsogram(str){
+  let arr = [];
+  let split = str.toLowerCase().split('');
+  
+  for(let i = 0; i < split.length; i++) {
+    if(arr.indexOf(split[i]) === -1) {
+      arr.push(split[i]);
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
